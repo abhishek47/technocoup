3c814d09bc2c45dc41cb5f9a330806355610d1d0
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-04-11 20:57:28 +0530

    Final Preloader 2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,9 +14,10 @@
 <!-- Responsive -->
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+<link href="/css/preloader.css" rel="stylesheet">
 <link href="/css/responsive.css" rel="stylesheet">
 
-<link href="/css/preloader.css" rel="stylesheet">
+
 
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
