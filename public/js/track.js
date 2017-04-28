837666df3c8490bd31067f2bb07c04c1e95a3eed
Abhishek Wani <Trumpets@Abhisheks-iMac.local>
2017-04-28 10:05:34 +0530

    Realtime

--- public/js/track.js
+++ public/js/track.js
@@ -141,7 +141,7 @@ function initialize() {
      if(coordinates[currentPolyline].length > 1){
      var lastPoint = coordinates[currentPolyline][coordinates[currentPolyline].length-1];
       var distance = google.maps.geometry.spherical.computeDistanceBetween (lastPoint, point);
-      if(distance > 50)
+      if(distance > 1000)
       {
          currentPolyline++;
          coordinates[currentPolyline] = [];
